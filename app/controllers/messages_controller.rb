3d9477558056cc905436df1fd5ddac970c4c8328
nekocreate <nekocreate7@gmail.com>
2015-09-11 04:23:34 +0000

    sakujo kinou kakunin

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,6 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update]
+  # before_action :set_message, only: [:edit, :update]
+  before_action :set_message, only: [:edit, :update, :destroy]
   
   def index
     # Messageを全て取得する
@@ -31,6 +32,12 @@ class MessagesController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
 
   private
   def message_params
@@ -38,7 +45,7 @@ class MessagesController < ApplicationController
   end
   
   def set_message
-  @message = Message.find(params[:id])
+    @message = Message.find(params[:id])
   end
 
 end
