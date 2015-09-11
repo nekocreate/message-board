3ff675d5b718f616dbd74a7b51da1c073cd0bb6d
nekocreate <nekocreate7@gmail.com>
2015-09-11 02:11:10 +0000

    hensyu kinou dousa kakunin ok

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,12 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+  
   def index
     # Messageを全て取得する
     @messages = Message.all
     @message = Message.new
   end
 
-  ## ここから追記
   def create
     @message = Message.new(message_params)
     if @message.save
@@ -18,10 +19,26 @@ class MessagesController < ApplicationController
     end
   end
   
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はTopページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は、編集画面に戻す
+      render 'edit'
+    end
+  end
+
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  ## ここまで
+  
+  def set_message
+  @message = Message.find(params[:id])
+  end
 
 end
